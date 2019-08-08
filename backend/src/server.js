38f44430e77d7c83b60dee5ffb902132191d1a70
Luis Eduardo Falcao <LuisEduardo.Falcao@saiglobal.com>
2019-08-07 23:53:56 -0300

    Creating Dev model and controller.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-qk4lt.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);

8fece82ab7330750c477d2e96b9ab7732354eec6
Luis Eduardo Falcao <LuisEduardo.Falcao@saiglobal.com>
2019-08-08 00:26:54 -0300

    Adding Like and Dislike endpoints

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,22 @@ const axios = require('axios');
 const Dev = require('../models/Dev')
 
 module.exports = {
+    async index (req, res) {
+        const { user } = req.headers;
+
+        const loggedDev = await Dev.findById(user);
+
+        const users = await Dev.find({
+            $and: [
+                { _id: { $ne: user } },
+                { _id: { $nin: loggedDev.likes} },
+                { _id: { $nin: loggedDev.dislikes} },
+            ],
+        });
+
+        return res.json(users);
+    },
+
     async store(req, res) {
 
         const { username } = req.body;
@@ -21,7 +37,7 @@ module.exports = {
             bio,
             avatar
         });
-        
+
         return res.json(dev);
     }
 }
